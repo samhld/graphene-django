adaef2b8acc9fd99d40f9f342b707c57bd64dd4b
Sam Dillard <sam@influxdata.com>
2019-09-06 17:49:51 -0700

    django boilerplate and server started; created Tracks app; created Track class/model in models.py

---
+++ app/tracks/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+#helper class that allows to build database model
+class Track(models.Model):
+    #below are fields that will map to particular database columns
+    title = models.CharField(max_length=20) #set max length of title field
+    description = models.TextField(blank=True) #blank=True makes this field optional
+    track_url = models.URLField() #URLField comes with validation of url
+    created_at = models.DateTimeField(auto_now_add=True)
+    #note that, when track is created, an id is automatically created
+    #id will serve as primary key for track
