49de350e8f2d5a658609e86dcd3a5b542a08b028
Sam Dillard <sam@influxdata.com>
2019-10-04 15:12:56 -0700

    added ability to authenticate as a user using JWT

--- app/app/schema.py
+++ app/app/schema.py
@@ -1,6 +1,7 @@
 import graphene
 import tracks.schema
 import users.schema
+import graphql_jwt
 
 # want to have schema defined in this higher level directory (/app/)
 # so create schema here and call the schema.py file objects from here (from tracks.schema, in this case)
@@ -8,7 +9,9 @@ class Query(users.schema.Query, tracks.schema.Query, graphene.ObjectType):
     pass
 
 class Mutation(users.schema.Mutation, tracks.schema.Mutation, graphene.ObjectType):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 # define schema here
 schema = graphene.Schema(query=Query, mutation=Mutation)
--- app/tracks/models.py
+++ app/tracks/models.py
@@ -7,7 +7,7 @@ class Track(models.Model):
     #below are fields that will map to particular database columns
     title = models.CharField(max_length=20) #set max length of title field
     description = models.TextField(blank=True) #blank=True makes this field optional
-    #url = models.URLField() #URLField comes with validation of url
+    url = models.URLField(default='http://tracks.com') #URLField comes with validation of url
     created_at = models.DateTimeField(auto_now_add=True)
     #note that, when track is created, an id is automatically created
     #id will serve as primary key for track
