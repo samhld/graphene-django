3886b8bbe15aed8a62683294cdc68c3dd23da10c
Sam Dillard <sam@influxdata.com>
2019-10-07 10:00:17 -0700

    added posted_by field to Track model

--- app/tracks/models.py
+++ app/tracks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
@@ -9,6 +10,7 @@ class Track(models.Model):
     description = models.TextField(blank=True) #blank=True makes this field optional
     url = models.URLField(default='http://tracks.com') #URLField comes with validation of url
     created_at = models.DateTimeField(auto_now_add=True)
+    posted_by = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE)
     #note that, when track is created, an id is automatically created
     #id will serve as primary key for track
 
