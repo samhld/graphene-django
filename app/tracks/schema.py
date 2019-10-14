3caeb910223c2f40cdee76005e9b218fae1ddc13
Sam Dillard <sam@influxdata.com>
2019-10-14 15:05:28 -0700

    added updateTrack capability that only allows the user who posted it to update it

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -22,10 +22,39 @@ class CreateTrack(graphene.Mutation):
         url = graphene.String()
 
     def mutate(self, info, title, description):
-        track = Track(title=title, description=description)
+        user = info.context.user
+
+        if user.is_anonymous:
+            raise Exception('Log into add a track.')
+
+        track = Track(title=title, description=description, posted_by=user)
         track.save()
         return CreateTrack(track=track)
+
+class UpdateTrack(graphene.Mutation):
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+        title = graphene.String()
+        description = graphene.String()
+        url = graphene.String()
+    
+    def mutate(self, info, track_id, title, url, description):
+        user = info.context.user
+        track = Track.objects.get(id=track_id)
+
+        if track.posted_by != user:
+            raise Exception('Not permitted to update track')
+
+        track.title = title
+        track.description = description
+        track.url = url
+        track.save()
+
+        return UpdateTrack(track=track)
 
 
 class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
+    update_track = UpdateTrack.Field()
